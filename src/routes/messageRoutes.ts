23931fec9a8b18c9dd3a6a0eadcbe422ac93333a
Hrithic <hrithicraj730@gmail.com>
2025-07-24 17:40:25 +0530

    send message converted to socket for realtime chating

--- src/routes/messageRoutes.ts
+++ src/routes/messageRoutes.ts
@@ -5,7 +5,7 @@ import { userAuth } from "../middleware/userAuth";
 const messageRouter = express.Router();
 
 messageRouter.get("/:chatId", userAuth, fetchMessages);
-messageRouter.post("/", userAuth, sendMessage);
+// messageRouter.post("/", userAuth, sendMessage);
 messageRouter.post("/read/:chatId", userAuth, markAsRead);
 
 export default messageRouter;
--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,9 @@
 import { Server } from "socket.io";
 import { Server as HttpServer } from "http";
 import dotenv from 'dotenv';
+import { Message } from "./models/messageModel";
+import { Chat } from "./models/chatModel";
+import User from "./models/userModel";
 
 dotenv.config();
 
@@ -20,10 +23,37 @@ const initializeSocket = (server: HttpServer)=>{
             socket.join(chatId);
         });
 
-        socket.on("sendMessage", (message)=>{
-            console.log("frontmsg", message)
-            // io.to(message.chatId).emit("receiveMessage", message);
+        // socket.on("sendMessage", (message)=>{
+        //     console.log("frontmsg", message)
+        //     // io.to(message.chatId).emit("receiveMessage", message);
+        // });
+
+        socket.on("sendMessage", async (messageData) => {
+            const { chatId, content, sender } = messageData;
+
+            // Save message to DB
+            const message = await Message.create({
+                chat: chatId,
+                sender: sender,
+                content,
+            });
+
+            // Update last message in chat
+            await Chat.findByIdAndUpdate(chatId, { lastMessage: message._id });
+
+            // Update unread count
+            const chat = await Chat.findById(chatId);
+            const receiverId = chat?.users.find((id) => id.toString() !== sender);
+            if (receiverId) {
+                await User.findByIdAndUpdate(receiverId, {
+                    $inc: { [`unreadMessages.${chatId}`]: 1 },
+                });
+            }
+
+            // Emit to all sockets in the room
+            io.to(chatId).emit("receiveMessage", message);
         });
+
 
         socket.on("disconnect", ()=>{
             console.log(`User Disconnected: ${socket.id}`);
