d287df10cf103380f6be7b260f2bbe1c614344a3
Hrithic <hrithicraj730@gmail.com>
2025-08-13 16:14:55 +0530

    Cloudinary and multer installed to handle image upload

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
 import User from "../models/userModel";
+import cloudinary from "../utils/cloudinaryConfig";
+// import { AuthenticatedRequest } from "../types/express";
 
 export const searchUsers = catchAsync(async (req: Request, res: Response) => {
     const { query } = req.query;
@@ -41,4 +43,36 @@ export const getUserProfile = catchAsync(async (req: Request, res: Response) => 
     const userId = req.user?._id;
     const userData = await User.findById(userId).select("_id name username profilePicture");
     res.status(200).json(userData);
+});
+
+export const updateProfile = catchAsync(async (req , res: Response) => {
+    const { name, username } = req.body;
+    const user = await User.findById(req.user?._id);
+    
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    // Handle image upload
+    if (req.file) {
+        const result = await cloudinary.uploader.upload(req.file.path, {
+            folder: 'chat-app/profiles',
+            width: 500,
+            crop: 'scale'
+        });
+        user.profilePicture = result.secure_url;
+    }
+
+    // Update user fields
+    user.name = name || user.name;
+    user.username = username || user.username;
+
+    await user.save();
+
+    res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        username: user.username,
+        profilePicture: user.profilePicture
+    });
 });
--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,7 +1,13 @@
-import { IUser } from "../interfaces/userInterface";
+import { User } from "../models/userModel";
+import { File } from "../types/multer";
 
 declare module "express" {
-    export interface Request {
-        user?: IUser;
-    }
+  export interface Request {
+    user?: User;
+    file?: File;
+  }
+}
+
+export interface AuthenticatedRequest extends Request {
+  user: User;
 }
