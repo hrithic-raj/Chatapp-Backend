ec1d1a66ef3475ec6866b0954247b637697f8687
Hrithic <hrithicraj730@gmail.com>
2026-01-12 15:22:11 +0530

    temporary authentication logic changed Instead of storing refresh token in cookies from backend cookies are passed from backend and stored from the frontend

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,13 +3,13 @@ import { NextFunction, Request, Response } from "express";
 import catchAsync from "../utils/catchAsync";
 import User from "../models/userModel";
 import { googleAuthService, refreshTokenService } from "../services/authServices";
-import { cookieSaver } from "../utils/jwtTokenGenerator";
+// import { cookieSaver } from "../utils/jwtTokenGenerator";
 import CustomError from "../utils/customErrorHandler";
 
 export const googleAuth = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const { credentialResponse } = req.body;
     const { user, accessToken, refreshToken, newUser } = await googleAuthService(res, credentialResponse);
-    cookieSaver(res, refreshToken);
+    // cookieSaver(res, refreshToken);
     
     res.status(200).json({
         status: "success",
@@ -46,7 +46,7 @@ export const refreshToken = catchAsync(async (req: Request, res: Response, next:
         return next(new CustomError('Refresh token required', 401));
     }
     const { newAccessToken, user } = await refreshTokenService(refreshToken);
-    cookieSaver(res, refreshToken);
+    // cookieSaver(res, refreshToken);
     res.status(200).json({
         status: "success",
         message: "New AccessToken created",
